f37866c95862e4d0f3ce741cbfeafe5bc64779db
daveseward <daveseward@asobi-k.com>
2017-02-13 13:03:22 +0000

    fix error in metadata

--- lib/logger_file_backend.ex
+++ lib/logger_file_backend.ex
@@ -96,7 +96,7 @@ defmodule LoggerFileBackend do
   defp format_event(level, msg, ts, md, %{format: format, metadata: keys}) do
     #Logger.Formatter.format(format, level, msg, ts, take_metadata(md, keys))
     pid_str = :io_lib.fwrite('~p', [md[:pid]]) |> to_string
-    JSON.encode! %{level: level, message: msg, pid: pid_str, module: md[:module], function: md[:function], line: md[:line], metadata: metadata}
+    JSON.encode! %{level: level, message: msg, pid: pid_str, module: md[:module], function: md[:function], line: md[:line], metadata: keys}
   end
 
   @doc false
